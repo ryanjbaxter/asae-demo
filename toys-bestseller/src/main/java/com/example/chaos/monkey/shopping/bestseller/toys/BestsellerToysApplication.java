855a555e74a60947e6b76279475a566f50a4a5aa
Ryan Baxter <524254+ryanjbaxter@users.noreply.github.com>
2023-07-19 17:08:41 -0400

    Adding refresh listener

--- toys-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/toys/BestsellerToysApplication.java
+++ toys-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/toys/BestsellerToysApplication.java
@@ -1,8 +1,14 @@
 package com.example.chaos.monkey.shopping.bestseller.toys;
 
+import de.codecentric.spring.boot.chaos.monkey.configuration.ChaosMonkeyProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +16,26 @@ public class BestsellerToysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BestsellerToysApplication.class, args);
+	}
+
+	@Configuration
+	public static class ChaosMonkeyRefreshListener implements ApplicationListener<EnvironmentChangeEvent> {
+		ChaosMonkeyProperties chaosMonkeyProperties;
+
+		Environment environment;
+
+		public ChaosMonkeyRefreshListener(ChaosMonkeyProperties chaosMonkeyProperties, Environment environment) {
+			this.chaosMonkeyProperties = chaosMonkeyProperties;
+			this.environment = environment;
+		}
+
+		@Override
+		public void onApplicationEvent(EnvironmentChangeEvent event) {
+
+			if(event.getKeys().contains("chaos.monkey.enabled")) {
+				chaosMonkeyProperties.setEnabled(environment.getProperty("chaos.monkey.enabled", Boolean.class));
+			}
+		}
 	}
 }
  
