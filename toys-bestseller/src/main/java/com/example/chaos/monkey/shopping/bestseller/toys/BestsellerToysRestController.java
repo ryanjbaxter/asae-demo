c7f14fa4f4edc02dc90ac72198a645e2a7a1acfb
Mark Heckler <mark.heckler@gmail.com>
2023-08-09 19:13:39 -0500

    Quick db enablement using CosmosDB for MongoDB in toys app

--- toys-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/toys/BestsellerToysRestController.java
+++ toys-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/toys/BestsellerToysRestController.java
@@ -1,42 +1,65 @@
 package com.example.chaos.monkey.shopping.bestseller.toys;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
-
 import com.example.chaos.monkey.shopping.domain.Product;
 import com.example.chaos.monkey.shopping.domain.ProductBuilder;
 import com.example.chaos.monkey.shopping.domain.ProductCategory;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Benjamin Wilms
  */
 @RestController
 public class BestsellerToysRestController {
+    private final ProductRepository repo;
 
     private String appid = UUID.randomUUID().toString();
 
-    @GetMapping("/toys/bestseller")
-    public List<Product> getBestsellerProducts(HttpServletResponse response) {
-        response.addHeader("appid", appid);
+    public BestsellerToysRestController(ProductRepository repo) {
+        this.repo = repo;
+    }
+
+    @PostConstruct
+    public void loadData() {
+        repo.deleteAll();
+
         AtomicLong aLong = new AtomicLong(1);
 
         ProductBuilder productBuilder = new ProductBuilder();
 
-        Product product1 = productBuilder.setCategory(ProductCategory.TOYS).setId(aLong.getAndIncrement()).setName("LEGO Star Wars Yodas Hut")
+        Product product1 = productBuilder
+                .setCategory(ProductCategory.TOYS)
+                .setId(aLong.getAndIncrement())
+                //.setName("LEGO Star Wars Yodas Hut")
+                .setName("LEGO Star Trek USS Enterprise")
                 .createProduct();
 
-        Product product2 = productBuilder.setCategory(ProductCategory.TOYS).setId(aLong.getAndIncrement()).setName("LEGO Star Wars Millennium Falcon")
+        Product product2 = productBuilder
+                .setCategory(ProductCategory.TOYS)
+                .setId(aLong.getAndIncrement())
+                //.setName("LEGO Star Wars Millennium Falcon")
+                .setName("LEGO Star Trek Romulan Bird of Prey")
                 .createProduct();
 
-        Product product3 = productBuilder.setCategory(ProductCategory.TOYS).setId(aLong.getAndIncrement()).setName("LEGO Star Wars Imperial Tie Fighter")
+        Product product3 = productBuilder
+                .setCategory(ProductCategory.TOYS)
+                .setId(aLong.getAndIncrement())
+                //.setName("LEGO Star Wars Imperial Tie Fighter")
+                .setName("LEGO Star Trek Klingon Battle Cruiser")
                 .createProduct();
-        return Arrays.asList(product1, product2, product3);
+
+        repo.saveAll(List.of(product1, product2, product3));
     }
 
+    @GetMapping("/toys/bestseller")
+    public Iterable<Product> getBestsellerProducts(HttpServletResponse response) {
+        response.addHeader("appid", appid);
+        return repo.findAll();
+    }
 }
