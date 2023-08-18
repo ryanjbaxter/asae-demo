c882f9e24c183f5fc1c56bc2a0c113da9239bb62
Mark Heckler <mark.heckler@gmail.com>
2023-08-17 20:19:37 -0500

    Refine DB config and cleanup scripts

--- asa-e/configDb.sh
+++ asa-e/configDb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Author  : Mark A. Heckler
 # Notes   : Run with 'source configDb.sh' from your shell/commandline environment
-# History : 20230810 Official "version 1"
 
 export ASAE_PREFIX=<Initials or other "me"fix that is easy to remember/find in the portal>
 export ASAE_LOCATION=<your region, e.g. eastus>
@@ -11,18 +10,18 @@ export ASAE_RESOURCE_GROUP=<your resource group>
 
 # Establish seed for random naming
 export RANDOMIZER=$RANDOM
-# export RANDOMIZER='3662'
+# export RANDOMIZER='6298'
 
 export COSMOSDB_MON_ACCOUNT=$ASAE_PREFIX'-'$RANDOMIZER'-mongoacct'
 export COSMOSDB_MON_NAME=$ASAE_PREFIX'-'$RANDOMIZER'-mongodb'
 
-# ONLY DO THE FOLLOWING SECTIONS IF REINIT TO USE EXISTING DBs! (see lines 10 and 11 above)
 ## Cosmos DB for MongoDB
 az cosmosdb create -n $COSMOSDB_MON_ACCOUNT -g $ASAE_RESOURCE_GROUP --kind MongoDB --server-version 4.0
 az cosmosdb mongodb database create -a $COSMOSDB_MON_ACCOUNT -n $ASAE_PREFIX'-my-test-db' -g $ASAE_RESOURCE_GROUP --verbose
 
 # For MongoDB API, a single URL connection string (URL+key)
-export COSMOSDB_MON_URL=$(az cosmosdb keys list --type connection-strings -n $COSMOSDB_MON_ACCOUNT -g $ASAE_RESOURCE_GROUP --query "connectionStrings[0].connectionString" --output tsv)
+# Not absolutely necessary, but useful for visual confirmation
+# export COSMOSDB_MON_URL=$(az cosmosdb keys list --type connection-strings -n $COSMOSDB_MON_ACCOUNT -g $ASAE_RESOURCE_GROUP --query "connectionStrings[0].connectionString" --output tsv)
 
 # Create Service Connector linking toys-bestseller app with Cosmos DB
 az spring connection create cosmos-mongo \
@@ -43,7 +42,7 @@ az spring connection create cosmos-mongo \
 # 1. Rebuild the app using:
 #    mvn clean package
 # 2. Redeploy the updated toys app using:
-#    az spring app deploy --resource-group $ASAE_RESOURCE_GROUP --service $ASAE_SERVICE --artifact-path toys-bestseller/target/toys-bestseller-1.0-SNAPSHOT.jar --name toys-bestseller
+#    az spring app deploy --resource-group $ASAE_RESOURCE_GROUP --service $ASAE_SERVICE --artifact-path ./target/toys-bestseller-1.0-SNAPSHOT.jar --name toys-bestseller
 
 # Now, go exercise those endpoints! Suggestion: hit the <gateway>/startpage a few times, refresh the app map, and chase the trace(s)!
 # *******************************************************************************
@@ -66,4 +65,3 @@ az spring connection create cosmos-mongo \
 ## Queries
 #    az spring app list -g $ASAE_RESOURCE_GROUP -s $ASAE_SERVICE --query "[].name"
 #    az spring app list -g $ASAE_RESOURCE_GROUP -s $ASAE_SERVICE --query "[].properties.activeDeployment.properties.status"
-
